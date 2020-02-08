b1d8d09137ff63acbe6325472c40e7aa616e17a4
Martin Schnurer <martin.schnurer@gmail.com>
2020-02-08 01:21:47 +0100

    comment out AWS config update

--- server/src/awsConfigure.ts
+++ server/src/awsConfigure.ts
@@ -1,9 +1,10 @@
 import AWS from "aws-sdk";
 
-AWS.config.update({
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-});
+
+// AWS.config.update({
+//   region: process.env.AWS_REGION,
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_KEY,
+// });
 
 export const documentClient = new AWS.DynamoDB.DocumentClient();
