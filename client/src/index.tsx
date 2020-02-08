1e3de7ce6d94851ae69c6cdeb7994797708d0fc4
Martin Schnurer <martin.schnurer@gmail.com>
2020-02-08 14:46:20 +0100

    format for consistency

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
 import { Provider } from "react-redux";
-import * as serviceWorker from './serviceWorker';
-import { store } from './redux/store';
-import 'typeface-open-sans';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { store } from "./redux/store";
+import "typeface-open-sans";
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- server/src/awsConfigure.ts
+++ server/src/awsConfigure.ts
@@ -1,10 +1,9 @@
 import AWS from "aws-sdk";
 
-
-// AWS.config.update({
-//   region: process.env.AWS_REGION,
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_KEY,
-// });
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_DYNAMODB_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_DYNAMODB_SECRET_KEY,
+});
 
 export const documentClient = new AWS.DynamoDB.DocumentClient();
